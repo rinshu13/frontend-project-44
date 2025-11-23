7e7bea0a7b02c4ceea920129bbc8e2f95bab2a9d
Renata Ilyasheva <rena.abi14gmail.com>
2025-11-23 19:46:07 +0300

    fix3

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,8 +5,8 @@ const OPERATIONS = ['+', '-', '*']
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-  case '+':
-    return num1 + num2
+    case '+':
+      return num1 + num2
     case '-':
       return num1 - num2
     case '*':
